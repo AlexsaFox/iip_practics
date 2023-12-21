f9a540c78a3291652a1e68cbda91f700f3764ae3
AlexsaFox <83453022+AlexsaFox@users.noreply.github.com>
2023-12-21 23:25:30 +0300

    latest changes - 2

--- modelstorage.py
+++ modelstorage.py
@@ -48,7 +48,7 @@ class ModelStorage:
         return values.count(0)
 
     def get_zero_count_for_all_fields(self):
-        fields = ['time', 'distance', 'iterations']
+        fields = ['trial', 'time', 'distance', 'iterations']
         return {fieldname: self._get_zero_count_for_field(fieldname) for fieldname in fields}
 
     def _get_algorithms_set(self):
